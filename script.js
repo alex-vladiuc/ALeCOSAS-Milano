dc5e92d7bf1ad7c99a9a0183c2c373570594a8ef
ItzA13x <alex.vladiuc@gmail.com>
2023-11-30 19:33:58 +0000

    English Version

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
-const toggleButton = document.getElementsByClassName("toggle-btn")[0]
-const navbarLinks = document.getElementsByClassName("page-link")[0]
+function collapseNav() {
+    let x = document.getElementById("navbarHeader");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+} 
 
-toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active")
-})
+
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+    const current = document.querySelector(".current");
+    current.classList.remove("current");
+    if(current.nextElementSibling) {
+        current.nextElementSibling.classList.add("current");
+    } else {
+        slides[0].classList.add("current");
+    }
+    setTimeout(() => current.classList.remove("current"))
+}
+
+const prevSlide = () => {
+    const current = document.querySelector(".current");
+    current.classList.remove("current");
+    if(current.previousElementSibling) {
+        current.previousElementSibling.classList.add("current");
+    } else {
+        slides[slides.length - 1].classList.add("current");
+    }
+    setTimeout(() => current.classList.remove("current"))
+}
+
+next.addEventListener("click", e => {
+    nextSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+prev.addEventListener("click", e => {
+    prevSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+
+if (auto) {
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
